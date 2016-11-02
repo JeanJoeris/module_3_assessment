80a8c1cd9350f7c32d35f2a0a801a09942d955c9
Jean Joeris <joeris.peter@gmail.com>
2016-11-02 12:01:53 -0600

    Refactor to generic faraday request

--- app/models/best_buy_service.rb
+++ app/models/best_buy_service.rb
@@ -2,27 +2,36 @@ class BestBuyService
   def self.by_zipcode(search_params)
     zip = search_params[:zip]
     distance = search_params[:distance]
-    response = conn.get do |req|
-      req.url "/v1/stores((area(#{zip},#{distance})))"
-      req.params['apiKey'] = ENV['best_buy_api_key']
-      req.params['pageSize'] = 15
-      req.params['format'] = 'json'
-    end
+    response = request(type: "area", zip: zip, distance: distance)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.by_store_id(store_id)
-    response = conn.get do |req|
-      req.url "/v1/stores((storeId=#{store_id}))"
-      req.params['apiKey'] = ENV['best_buy_api_key']
-      req.params['pageSize'] = 15
-      req.params['format'] = 'json'
-    end
+    response = request(type: "store_id", store_id: store_id)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
     def self.conn
       conn = Faraday.new(url: "https://api.bestbuy.com")
+    end
+
+    def self.request(params)
+      function = area_function(params[:zip], params[:distance]) if params[:type] == 'area'
+      function = store_id_function(params[:store_id]) if params[:type] == 'store_id'
+      conn.get do |req|
+        req.url "/v1/stores#{function}"
+        req.params['apiKey'] = ENV['best_buy_api_key']
+        req.params['pageSize'] = 15
+        req.params['format'] = 'json'
+      end
+    end
+
+    def self.area_function(zip, distance)
+      "((area(#{zip},#{distance})))"
+    end
+
+    def self.store_id_function(store_id)
+      "((storeId=#{store_id}))"
     end
 end
