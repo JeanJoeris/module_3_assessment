c55b8f3552f62406f447c96ae3522be4d6fe95ac
Jean Joeris <joeris.peter@gmail.com>
2016-11-02 09:15:07 -0600

    Add test for api item show

---
+++ spec/requests/item_show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Item API' do
+  it "shows a single item" do
+    item = create(:item)
+
+    get "/api/v1/items/#{item.id}"
+
+    response_item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_item[:name]).to eq("Frozen Banana")
+    expect(response_item[:description]).to eq("Bluth Original")
+    expect(response_item[:image_url]).to eq("http://vignette1.wikia.nocookie.net/arresteddevelopment/images/c/cd/1x05_Charity_Drive_(16).png")
+  end
+end
