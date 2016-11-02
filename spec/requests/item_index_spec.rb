73aadb3f17fd23932c95f82576343242ec67b915
Jean Joeris <joeris.peter@gmail.com>
2016-11-02 09:07:46 -0600

    Add test for api item index

---
+++ spec/requests/item_index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Item API' do
+  it "finds index of items as json" do
+    create_list(:item, 4)
+
+    get "/api/v1/items"
+
+    response_items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_items.count).to eq(4)
+    expect(response_items.first[:name]).to eq("Frozen Banana")
+  end
+end
---
+++ spec/support/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    name "Frozen Banana"
+    description "Bluth Original"
+    image_url "http://vignette1.wikia.nocookie.net/arresteddevelopment/images/c/cd/1x05_Charity_Drive_(16).png"
+  end
+end
