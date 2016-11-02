fa7aea504330a04ffb9faec3142746fce1355f02
Jean Joeris <joeris.peter@gmail.com>
2016-11-02 10:14:20 -0600

    Finish user searches store feature, needs refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     response = Faraday.get("https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV['best_buy_api_key']}&pageSize=15&format=json")
-    JSON.parse(response.body, symbolize_names: true)
+    @data = JSON.parse(response.body, symbolize_names: true)
   end
 
   private
