f832da670f231c5bb422f3706b07e5dfc433ec45
Jean Joeris <joeris.peter@gmail.com>
2016-11-02 09:21:15 -0600

    Add test for api deleting item

---
+++ spec/requests/delete_item_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'Item API' do
+  it "deletes an item" do
+    item = create(:item)
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(Item.count).to eq(0)
+    expect(response.status).to eq(200)
+  end
+end
