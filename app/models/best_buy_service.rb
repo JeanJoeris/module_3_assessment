55265bc8cc123201997a7e5344820b2aea82242c
Jean Joeris <joeris.peter@gmail.com>
2016-11-02 11:53:43 -0600

    Refactor faraday connection

--- app/models/best_buy_service.rb
+++ app/models/best_buy_service.rb
@@ -2,12 +2,27 @@ class BestBuyService
   def self.by_zipcode(search_params)
     zip = search_params[:zip]
     distance = search_params[:distance]
-    response = Faraday.get("https://api.bestbuy.com/v1/stores((area(#{zip},#{distance})))?apiKey=#{ENV['best_buy_api_key']}&pageSize=15&format=json")
+    response = conn.get do |req|
+      req.url "/v1/stores((area(#{zip},#{distance})))"
+      req.params['apiKey'] = ENV['best_buy_api_key']
+      req.params['pageSize'] = 15
+      req.params['format'] = 'json'
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.by_store_id(store_id)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores((storeId=#{store_id}))?apiKey=#{ENV['best_buy_api_key']}&pageSize=15&format=json")
+    response = conn.get do |req|
+      req.url "/v1/stores((storeId=#{store_id}))"
+      req.params['apiKey'] = ENV['best_buy_api_key']
+      req.params['pageSize'] = 15
+      req.params['format'] = 'json'
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  private
+    def self.conn
+      conn = Faraday.new(url: "https://api.bestbuy.com")
+    end
 end
